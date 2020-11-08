476b0ee32292dbb6c415fdc11a4f8fd903cb92d8
Shin seongsu <tlstjdtn321@naver.com>
2020-11-08 22:05:16 +0900

    select 연습

--- tuning3.sql
+++ tuning3.sql
@@ -13,3 +13,32 @@ SELECT I.ITEM_ID            상품ID
        )                    상품구분명
   FROM ITEM I
  WHERE I.ITEM_TYPE_CD = '100101';
+
+
+
+
+SELECT I.ITEM_ID            상품ID
+     , I.ITEM_NM            상품명
+     , UI.UITEM_NM          단품명
+     , I.ITEM_TYPE_CD       상품구분코드
+     , ( SELECT UNI_CD_NM 
+           FROM COM_CD 
+          WHERE UNI_CD = I.ITEM_TYPE_CD 
+        )                   상품구분명
+  FROM ITEM I
+     , UITEM UI
+ WHERE I.ITEM_TYPE_CD = '100101'
+   AND I.ITEM_ID = UI.ITEM_ID;
+   
+   
+
+SELECT I.ITEM_ID
+     , I.ITEM_NM
+     , SUM(UP.SALE_PRICE)
+  FROM ITEM I
+     , UITEM_PRICE UP
+ WHERE I.ITEM_TYPE_CD = '100101'
+   AND I.ITEM_ID = UP.ITEM_ID
+ GROUP BY I.ITEM_ID
+        , I.ITEM_NM
+ ORDER BY 1;
